8242060d2fa8ceef61daff2ab65517e4ed27d910
Diego <diegom.develops@gmail.com>
2021-07-15 11:59:50 -0400

    Second Commit

---
+++ challenge.py
@@ -0,0 +1,24 @@
+from proyect import DATA
+all_python_devs = list(filter(lambda worker: worker['language'] == 'python', DATA))
+all_python_devs = list(map(lambda worker: worker['name'], all_python_devs))
+all_Platzi_workers = list(filter(lambda worker: worker['organization'] == 'Platzi', DATA))
+all_Platzi_workers = list(map(lambda worker: worker['name'], all_Platzi_workers))
+adults = [worker['name'] for worker in DATA if worker['age'] > 18]
+old_people = [worker | {'old': worker['age'] < 70} for worker in DATA]
+
+
+# adults = [worker['name'] for worker in DATA if worker['age'] > 18]
+# # older = [worker | {'old': worker['age'] > 70} for worker in DATA]
+# print(adults)
+print(all_python_devs)
+print(all_Platzi_workers)
+print(adults)
+for worker in old_people:
+    print(worker)
+#for worker in older:
+    #print(older)
+    
+# oll_people = [worker | {"oll": worker['age'] > 70} for worker in DATA]
+
+# for worker in oll_people:
+#     print(worker)
--- list.py
+++ list.py
@@ -5,10 +5,17 @@ def main():
     #         my_list.append(i ** 2)
     
     my_list = [i**2 for i in range(1, 10) if i % 3 != 0]
-    list = [i for i in range(1, 99999) if i % 36 == 0]
-            
+    list2 = [i for i in range(1, 99999) if i % 36 == 0]
+    list1 = [1, 4, 5, 6, 9, 13, 19, 21]
+    list3 = [i for i in list1 if i % 2 != 0 ]
+    odd = list(filter(lambda x: x%2 != 0, list1))
+    b = [2, 2, 2, 2, 2]
+    add = [i**i for i in b]
+    print(odd)
+    print(list3)
+    print(add)
+
                     
-    print(list)
 
 if __name__ == '__main__':
     main()
